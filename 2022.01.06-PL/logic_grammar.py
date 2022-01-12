767d2717a9f0e61d1f9ecc1c7773ec12e2a68176
Pedro Simões <69718437+L0ud3r@users.noreply.github.com>
2022-01-12 12:41:58 +0000

    Documentação de código em lexer e grammar

--- 2022.01.06-PL/logic_grammar.py
+++ 2022.01.06-PL/logic_grammar.py
@@ -8,11 +8,15 @@ from logic_eval import LogicEval
 #TODO:
 # Declarar variaveis apenas apos o inicio
 # Remover debug prints (ANTES DE APRESENTAR!)
-# Documentação
+# Documentação (In progress...) [VER LINHA 182, 196, 255]
 # Relatório
+# Mudar nome de certas classes e métodos
 
 
+# Classe referente à gramática utilizada na interpretação do pseudocódigo em Portugol
 class LogicGrammar:
+
+    # Lista de com as operações por ordem de prioridade
     precedence = (
 
         ("left", "or", "xor"),  # menor prioridade
@@ -26,33 +30,35 @@ class LogicGrammar:
 
     )
 
+    # Método com mensagens de controlo de erros inesperados
     def p_error(self, p):
         if p:
             raise Exception(f"Parse Error: Unexpected token '{p.type}'")
         else:
             raise Exception("Parse Error: Expecting token")
 
-    #
-
+    # Método com a regra inicial da gramática implementada, sendo que pode haver funções declaradas acima do "main"
     def p_portugol(self, p):
         """portugol : func_list ';' inicio code"""
         p[0] = [p[1]] + [p[4]]
 
+    # Método com outra via da regra inicial da gramática implementada, sem funções previamente declaradas
     def p_portugol2(self, p):
         """portugol : inicio code"""
         p[0] = [p[2]]
 
-    #
-
+    # Método com a regra de ler funções ou múltiplas funções
     def p_func_list(self, p):
         """func_list : func
                     | func_list ';' func"""
+        # Dependendo do tamanho do p, irá interpretar de forma diferente
         if len(p) == 2:
             p[0] = [p[1]]
         else:
             p[0] = p[1]
             p[0].append(p[3])
 
+    # Método com a regra de leitura de declaração e implementação função
     def p_func(self, p):
         """ func : funcao var '(' args ')' com_list ';' fimfuncao """
         p[0] = {
@@ -61,25 +67,23 @@ class LogicGrammar:
             "data": [p[2], p[4], p[6]]
         }
 
-    #
-
+    # Método com a regra de leitura de código, sendo que esta regra assume apenas 1 estrutura de código
     def p_code1(self, p):
         """ code : s """
         p[0] = [p[1]]
 
+    # Método com a regra de leitura de código, sendo que esta regra assume múltiplas estrutura de código
     def p_code2(self, p):
         """ code : code ';' s  """
         p[0] = p[1] + [p[3]]
 
-    #
-
+    # Método com a regra de definição de fim do "main" ou continuação de código
     def p_s(self, p):
         """ s : comando
               | fim"""
         p[0] = p[1]
 
-    #
-
+    # Método com a regra de leitura do ciclo for() em Portugol
     def p_ciclo(self, p):
         """ ciclo : para var de e ate e faca com_list ';' fimpara """
         p[0] = {
@@ -88,6 +92,7 @@ class LogicGrammar:
             "data": [p[8]],
         }
 
+    # Método com a regra de leitura do while() em Portugol
     def p_ciclo2(self,p):
         """ciclo : enquanto n faca com_list ';' fimenquanto"""
         p[0] = {
@@ -96,46 +101,49 @@ class LogicGrammar:
             "data": [p[2], p[4]]
         }
 
-    #
-
+    # Método com a regra de leitura de comandos, sendo estes atribuição de valores a variaveis ou criação de ciclos
     def p_comando1(self, p):
         """ comando : e
                     | ciclo """
         p[0] = p[1]
 
+    # Método com a regrad e leitura de declaração de variáveis
     def p_comando2(self, p):
         """ comando : var assign e """
         p[0] = {"op": "assign", "args": [p[1], p[3]]}
 
-
+    # Método com a regra de leitura do print() em Portugol
     def p_comando3(self, p):
         """ comando : escreva '(' e_list ')' """
         p[0] = {"op": "escreva", "args": p[3]}
 
-
+    # Método com a regra de leitura do input() em Portugol
     def p_comando4(self, p):
         """ comando : leia '(' var_list ')' """
         p[0] = {"op": "leia", "args":  p[3]}
 
+    # Método com a regra de leitura de declaração de múltiplas variáveis
     def p_comando5(self, p):
         """ comando : vartype ':' var_list """
         p[0] = {"op": "declarar",
                 "args": p[3],
                 "data": [p[1]]}
 
+    # Método com a regra de leitura do if() -> else() em Portugol
     def p_comando6(self, p):
         """comando : se e entao com_list ';' senao com_list ';' fimse"""
         p[0] = {"op": "se",
                 "args": [p[2]],
                 "data": [p[4], p[7]]}
 
+    # Método com a regra de leitura do if() em Portugol
     def p_comando7(self, p):
         """comando : se e entao com_list ';' fimse"""
         p[0] = {"op": "se",
                 "args": [p[2]],
                 "data": [p[4]]}
-    #
 
+    # Método com a regra de definição de múltiplos comandos (ciclos e declarações) ou apenas 1 comando
     def p_com_list(self, p):
         """ com_list : comando
                      | com_list ';' comando """
@@ -145,8 +153,7 @@ class LogicGrammar:
             p[0] = p[1]
             p[0].append(p[3])
 
-    #
-
+    # Método com a regra de leitura de várias atribuições de valores ou apenas 1 atribuição
     def p_e_list(self, p):
         """ e_list : e
                    | e_list ',' e """
@@ -155,22 +162,24 @@ class LogicGrammar:
         else:
             p[0] = p[1] + [p[3]]
 
-    #
-
+    #  Método com regra de leitura de atribuir como valor a uma variável outra variável
     def p_e1(self, p):
         """ e : var """
         p[0] = {'var': p[1]}
 
+    #  Método com regra de leitura de atribuir como valor a uma variável, outra atribuição dentro de parenteses
     def p_e2(self, p):
         """ e : '(' e ')' """
         p[0] = p[2]
 
+    #  Método com regra de leitura de atribuir como valor a uma variável um bool, uma string ou um número
     def p_e3(self, p):
         """ e : b
               | n
               | string """
         p[0] = p[1]
 
+    #  Método com regra de leitura de atribuir como valor a uma variável ... (??????????????????????????????????)
     def p_e4(self, p):
         """ e : var '(' e_list ')'
               | var '(' ')' """
@@ -178,17 +187,18 @@ class LogicGrammar:
                 "args": [],
                 "data": [p[1], [] if p[3] == ")" else p[3]]}
 
-    #
-
+    #  Método com regra de leitura de um número sendo positivo ou negativo
     def p_n1(self, p):
         """ n : nr
               | '-' e  %prec uminus  """
         p[0] = p[1] if len(p) == 2 else {"op": "-", "args": [0.0, p[2]]}
 
+    #  Método com regra de leitura de um número sendo ... (??????????????????????????????????????)
     def p_n2(self, p):
         """ n : '(' n ')' """
         p[0] = p[2]
 
+    # Método com regra de leitura de um número sendo o resultado de uma operação aritmética
     def p_n3(self, p):
         """ n : e '+' e
               | e '-' e
@@ -196,6 +206,7 @@ class LogicGrammar:
               | e '/' e """
         p[0] = dict(op=p[2], args=[p[1], p[3]])
 
+    # Método com regra de leitura de um número sendo o resultado de uma operação lógica
     def p_n4(self, p):
         """n : e '<' e
               | e leq e
@@ -205,8 +216,7 @@ class LogicGrammar:
               | e dif e"""
         p[0] = dict(op=p[2], args=[p[1], p[3]])
 
-    #
-
+    # Método com regra de leitura de um boolean value com valor dele própria ou de uma expressão booleana
     def p_b1(self, p):
         """ b : f
               | e or e
@@ -217,22 +227,22 @@ class LogicGrammar:
         else:
             p[0] = dict(op=p[2], args=[p[1], p[3]])
 
-    #
-
+    # Método com regra de leitura de um boolean value com valor dele como true
     def p_f1(self, p):
         """ f : true """
         p[0] = True
 
+    # Método com regra de leitura de um boolean value com valor dele como false
     def p_f2(self, p):
         """ f : false """
         p[0] = False
 
+    # Método com regra de leitura de um boolean value com valor inverso a ele mesmo
     def p_f3(self, p):
         """ f : not f """
         p[0] = dict(op="not", args=[p[2]])
 
-    #
-
+    # Método com regra de leitura de uma variável sozinha ou múltiplas variáveis
     def p_var_list(self, p):
         """ var_list : var
                      | var_list ',' var """
@@ -242,18 +252,19 @@ class LogicGrammar:
             p[0] = p[1]
             p[0].append(p[3])
 
-    #
-
+    # Método com regra de leitura de ... (????????????????????????????????????????) ESQUECI-ME xD
     def p_args(self, p):
         """ args :
                  | var_list """
         p[0] = p[1] if len(p) == 2 else []
 
+    # Método inicializador da classe com os seus atributos declarados
     def __init__(self):
         self.lexer = LogicLexer()
         self.tokens = self.lexer.tokens
         self.yacc = pyacc.yacc(module=self)
 
+    # Método de execução do Parse do texto no documento alvo (Exercução do funcionamento do Grammar)
     def parse(self, expression):
         ans = self.yacc.parse(lexer=self.lexer.lex, input=expression)
         pp = PrettyPrinter()
--- 2022.01.06-PL/logic_lexer.py
+++ 2022.01.06-PL/logic_lexer.py
@@ -2,6 +2,7 @@
 import ply.lex as plex
 
 
+# Classe referente ao lexer e às linguagens utilizadas para ler o ficheiro em pseudocódigo Portugol
 class LogicLexer:
     #TODO organizar
     keywords = ("inicio", "true", "false", "not", "and", "or", "xor", "leq", "geq", "dif", "para", "de", "ate", "faca", "fimpara", "enquanto", "fimenquanto", "se", "senao", "entao", "fimse", "escreva", "leia", "funcao", "fimfuncao", "fim")
@@ -9,20 +10,24 @@ class LogicLexer:
     literals = "()+-<>!=/*;[],:"
     t_ignore = " \t\n"
 
+    # Método com expressão regular do comentário
     def t_comment(self, t):
         r"""\#.*"""
         pass
 
+    # Método com expressão regular capaz de ler um valor de string
     def t_string(self, t):
         r'"[^"]*"'
         t.value = t.value[1:-1]
         return t
 
+    # Método com expressão regular capaz de ler um valor de número inteiro ou real
     def t_nr(self, t):
         r"""[0-9]+(\.[0-9]+)?"""
         t.value = float(t.value)
         return t
 
+    # Método com expressão regular capaz de ler os diferentes tipos de variáveis
     def t_vartype(self, t):
         r"""inteiro
             |caracter
@@ -30,33 +35,43 @@ class LogicLexer:
             |real"""
         return t
 
+    # Método com expressão regular capaz de ler o sinal de atruibuição de valores a variaveis em Portugol
     def t_assign(self, t):
         r"""<-"""
         return t
 
+    # Método com expressão regular capaz de ler o simbolo de lower or equal (menor ou igual)
     def t_leq(self,t ):
         r""""<="""
         return t
 
+    # Método com expressão regular capaz de ler o simbolo de greater or equal (maior ou igual)
     def t_geq(self, t):
         r""">="""
         return t
 
+    # Método com expressão regular capaz de ler o simbolo de diferente
+    # (o símbolo de igual lógico ficou designado como '=', não sendo necessária uma expressão regular para o mesmo)
     def t_dif(self, t):
         r"""!="""
         return t
 
+    # Método com expressão regular capaz de ler o simbolo de ler todos os tipos de keywords presentes no lexer
     def t_keywords(self, t):
         r"""[a-z]+"""
+        # No caso da palavra não estar nas keywords, estão assume-se como o nome de uma variável
         t.type = t.value if t.value in self.keywords else "var"
         return t
 
+    # Método de controlo de erros no lexer
     def t_error(self, t):
         raise Exception(f"Unexpected token {t.value[:10]}")
 
+    # Método inicializador da classe com o seu atributo lex inicializado
     def __init__(self):
         self.lex = plex.lex(module=self)
 
+    # Método de execução do lexer no documento
     def token(self):
         return self.lex.token()
 
